700eeb130b1cc084ef4e46314058c0a3966d60b4
TonyJem <a.jemanov@gmail.com>
2021-07-02 21:20:25 +0300

    Update ImageCollectionViewController

--- GaleryView/GaleryView/ImageCollectionViewController.swift
+++ GaleryView/GaleryView/ImageCollectionViewController.swift
@@ -1,9 +1,23 @@
 import UIKit
 
 final class ImageCollectionViewController: UICollectionViewController {
-
+    
+    @IBOutlet private weak var searchTextField: UITextField!
+    @IBOutlet private weak var clearSearchButton: UIBarButtonItem!
+    
+    private let reuseIdentifier = "ImageCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        searchTextField.delegate = self
     }
+    
+    @IBAction func clearSearchButtonAction(_ sender: UIBarButtonItem) {
+        print("🟢 Clear Search button did tapped!")
+        searchTextField.text = ""
+    }
+}
+
+extension ImageCollectionViewController: UITextFieldDelegate {
 }
