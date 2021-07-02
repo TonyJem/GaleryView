738cb2c0d5b46cf8b10ae822c4e5fcd63aa72e04
TonyJem <a.jemanov@gmail.com>
2021-07-02 21:51:07 +0300

    Update ImageCollectionViewController

--- GaleryView/GaleryView/ImageCollectionViewController.swift
+++ GaleryView/GaleryView/ImageCollectionViewController.swift
@@ -6,6 +6,9 @@ final class ImageCollectionViewController: UICollectionViewController {
     @IBOutlet private weak var clearSearchButton: UIBarButtonItem!
     
     private let reuseIdentifier = "ImageCell"
+    private let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+    private var searches: [FlickrSearchResults] = []
+    private let flickr = Flickr()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,5 +22,13 @@ final class ImageCollectionViewController: UICollectionViewController {
     }
 }
 
+// MARK: - Private
+private extension ImageCollectionViewController {
+  func image(for indexPath: IndexPath) -> FlickrImage {
+    return searches[indexPath.section].searchResults[indexPath.row]
+  }
+}
+
+// MARK: - Search TextField Delegate
 extension ImageCollectionViewController: UITextFieldDelegate {
 }
